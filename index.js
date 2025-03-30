1ca0dabe557ca7e3538b52a40c6b5a27f887bad1
cuongbd <bdcuongvn83git@gmail.com>
2025-03-30 15:48:33 +1100

    fix bug
    Signed-off-by: cuongbd <bdcuongvn83git@gmail.com>

--- index.js
+++ index.js
@@ -25,8 +25,9 @@ app.post("/api/shorturl", (req, res) => {
   const { url } = req.body;
   //http://www.example.com
   //var regex = /^\d{4}-\d{2}-\d{2}$/;
-  var regex = /^https:(\/\/)[^.]+\.[^.]+$/;
+  const regex = /^(https?:\/\/)(www\.)?[\w-]+\.[a-z]{2,6}(\.[a-z]{2})?$/i;
   console.log(`url:${url}`);
+
   let valid = regex.test(url);
   console.log(`regex.test(url):${valid}`);
 
